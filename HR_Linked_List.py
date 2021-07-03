3bb031481343ec74929a35f53fccbc780bbbbd64
shahbazakon <57652434+shahbazakon@users.noreply.github.com>
2021-07-03 14:23:33 +0530

    add Liked List insertion program

---
+++ HR_Linked_List.py
@@ -0,0 +1,38 @@
+# ===================================================================
+"""
+Complete the insert function in your editor so that it creates a
+new Node (pass data as the Node constructor argument) and inserts it
+at the tail of the linked list referenced by the head parameter.
+Once the new node is added, return the reference to the noda node.
+"""
+# ===================================================================
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Solution:
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self, head, data):
+        if head is None:
+            head = Node(data)
+            self.tail = head
+        else:
+            node = Node(data)
+            self.tail.next = node
+            self.tail = node
+        return head
+
+
+Lst= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=Lst.insert(head, data)
+Lst.display(head);
+# ===================================================================
