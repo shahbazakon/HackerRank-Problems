5eeb608bd919284d60f781df8dfdc13a73543c42
shahbazakon <57652434+shahbazakon@users.noreply.github.com>
2021-07-18 16:06:53 +0530

    add RegEx

---
+++ HR_RegEx.py
@@ -0,0 +1,35 @@
+# =============================================================================
+"""
+Task
+Consider a database table, Emails, which has the attributes First Name and
+Email ID. Given N rows of data simulating the Emails table, print
+an alphabetically-ordered list of people whose email address ends in @gamil.com.
+"""
+# =============================================================================
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    # Dictnory = {}
+    lst = []
+    for N_itr in range(N):
+        first_multiple_input = input().rstrip().split()
+
+        firstName = first_multiple_input[0]
+        emailID = first_multiple_input[1]
+        if re.search('@gmail\.com$', emailID):
+            lst.append(firstName)
+    print(*sorted(lst), sep='\n')
+
+    #     mail = re.findall('[a-z]+', emailID)
+    #     Dictnory[mail[0]] = firstName
+    #
+    # for i in sorted(Dictnory.values()):
+    #     print(i)
+# =============================================================================
